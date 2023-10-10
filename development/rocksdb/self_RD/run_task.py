82e9f7a792110b34061ed20b391b9d20e21fe413
YCHuang2112 <96855801+YCHuang2112@users.noreply.github.com>
2023-10-11 02:25:03 +0800

    modify run_workload.py

--- development/rocksdb/self_RD/run_task.py
+++ development/rocksdb/self_RD/run_task.py
@@ -1,7 +1,7 @@
 import os
 params = {
-    "-i": [100000*10],
-    # "-i": [100000],
+    #"-i": [100000*10],
+    "-i": [100000],
     "-P": [512],
     "-T": [2],
     "--insert_before_range_delete": [0.9],
@@ -9,8 +9,8 @@ params = {
     #"--max_open_files": [999],
     "--max_open_files": [20],
 	"--skip_reading_RD_blocks": [0],
-	"--number_of_PQ": [5000*100],
-	# "--number_of_PQ": [5000],
+	#"--number_of_PQ": [5000*100],
+	"--number_of_PQ": [5000],
 }
 tasks = ["./simple_example"]
 for key, values in params.items():
@@ -32,20 +32,20 @@ def run(
     else:
         RD_list = RD_list * len(sel_list)
 
-    for i, task in enumerate(tasks):
+    for i, task_orig in enumerate(tasks):
         for j, (RD, sel) in enumerate(zip(RD_list, sel_list)):
             file_code = exp_code+str(i+1)+str(j+1)
-            task += f" --RD {RD} --selectivity {sel} --workload_filename workload/workload{file_code}.txt > log{file_code}"
+            task = task_orig + f" --RD {RD} --selectivity {sel} --workload_filename workload/workload{file_code}.txt > log{file_code}"
             print(task)
             os.system(task)
         
         
-#run('4', RD_list=[1, 5, 10, 50, 100, 500, 900], sel_list=[0.001])
+#run('4', RD_list=[0, 1, 5, 10, 50, 100, 500, 900], sel_list=[0.001])
 #run('5', RD_list=[100], sel_list=[0.0001, 0.0005, 0.001, 0.005, 0.009])
 #run('6', RD_list=[10], sel_list=[0.001, 0.005, 0.009, 0.01, 0.05, 0.09])
-# run('7', RD_list=[50], sel_list=[0.001, 0.005, 0.009, 0.01])
-# run('8', RD_list=[50], sel_list=[0.001, 0.005, 0.009, 0.01])
-# run('9', RD_list=[50], sel_list=[0.001, 0.005, 0.009, 0.01])
-run('7', RD_list=[0], sel_list=[0.001, 0.005, 0.009, 0.01])
-run('8', RD_list=[0], sel_list=[0.001, 0.005, 0.009, 0.01])
-run('9', RD_list=[0], sel_list=[0.001, 0.005, 0.009, 0.01])
+run('7', RD_list=[50], sel_list=[0.001, 0.005, 0.009, 0.01])
+run('8', RD_list=[50], sel_list=[0.001, 0.005, 0.009, 0.01])
+run('9', RD_list=[50], sel_list=[0.001, 0.005, 0.009, 0.01])
+#run('7', RD_list=[0], sel_list=[0.001, 0.005, 0.009, 0.01])
+#run('8', RD_list=[0], sel_list=[0.001, 0.005, 0.009, 0.01])
+#run('9', RD_list=[0], sel_list=[0.001, 0.005, 0.009, 0.01])
