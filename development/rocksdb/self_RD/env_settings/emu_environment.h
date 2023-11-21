fe2a7572455d087d20e03758fa9b9d3e495e224a
Shubham Kaushik <kaushiks@bu.edu>
2023-11-21 23:39:05 +0000

    Adds experiments results

--- development/rocksdb/self_RD/env_settings/emu_environment.h
+++ development/rocksdb/self_RD/env_settings/emu_environment.h
@@ -87,7 +87,11 @@ public:
     int level0_file_num_compaction_trigger; // number of files that triggers compaction in level0
     int target_file_size_multiplier; // multiplier for file size if we want to have same number of files per level
     int max_background_jobs; // Maximum number of concurrent background jobs (compactions and flushes).
+
+    bool level_compaction_dynamic_file_size; // dynamic file sizes for level compaction  
     int max_compaction_bytes; // target_file_size_base * 25, limit number of bytes in one compaction to be lower than this threshold. But it’s not guaranteed.
+    bool ignore_max_compaction_bytes_for_input; // [def:false] 
+
     long max_bytes_for_level_base; // capacity for level base(usually level 1)
     int merge_operator;   // TBC
     int soft_pending_compaction_bytes_limit;    // TBC
@@ -320,6 +324,9 @@ public:
   long file_system_page_size;
   bool only_tune;
   int num_read_query_sessions;
+
+
+  friend ostream& operator<<(ostream& os, const EmuEnv& _env);
 };
 
 #endif /*EMU_ENVIRONMENT_H_*/
--- development/rocksdb/self_RD/run_task.py
+++ development/rocksdb/self_RD/run_task.py
@@ -2,10 +2,11 @@ import os
 params = {
     #"-i": [100000*10],
     "-i": [100000],
-    "-P": [512],
+    # "-P": [512],
+    "-P": [4],
     "-T": [2],
     "--insert_before_range_delete": [0.9],
-    "--gen_workload": [1],
+    "--gen_workload": [0],
     #"--max_open_files": [999],
     "--max_open_files": [20],
     "--skip_reading_RD_blocks": [0],
@@ -17,8 +18,10 @@ tasks = ["./simple_example"]
 for key, values in params.items():
     tasks = [" ".join([task, key, str(value)]) for value in values for task in tasks]
 
-B_list: list = [4, 16, 64, 256]
-E_list: list = [1024, 256, 64, 16]
+# B_list: list = [4, 16, 64, 256]
+# E_list: list = [1024, 256, 64, 16]
+B_list: list = [256]
+E_list: list = [16]
 
 task_tmp = []
 for i, (B, E) in enumerate(zip(B_list, E_list)):
@@ -48,9 +51,17 @@ def run(
 #run('4', RD_list=[0, 1, 5, 10, 50, 100, 500, 900], sel_list=[0.001])
 #run('5', RD_list=[100], sel_list=[0.0001, 0.0005, 0.001, 0.005, 0.009])
 #run('6', RD_list=[10], sel_list=[0.001, 0.005, 0.009, 0.01, 0.05, 0.09])
-run('7', RD_list=[50], sel_list=[0.001, 0.005, 0.009, 0.01])
-run('8', RD_list=[50], sel_list=[0.001, 0.005, 0.009, 0.01])
-run('9', RD_list=[50], sel_list=[0.001, 0.005, 0.009, 0.01])
+# run('7', RD_list=[50], sel_list=[0.001, 0.005, 0.009, 0.01])
+# run('8', RD_list=[50], sel_list=[0.001, 0.005, 0.009, 0.01])
+# run('9', RD_list=[50], sel_list=[0.001, 0.005, 0.009, 0.01])
 #run('7', RD_list=[0], sel_list=[0.001, 0.005, 0.009, 0.01])
 #run('8', RD_list=[0], sel_list=[0.001, 0.005, 0.009, 0.01])
 #run('9', RD_list=[0], sel_list=[0.001, 0.005, 0.009, 0.01])
+# run('20', RD_list=[100], sel_list=[0.001])
+# run('21', RD_list=[100], sel_list=[0.001])
+# run('22', RD_list=[100], sel_list=[0.001])
+# run('23', RD_list=[100], sel_list=[0.001])
+run('24', RD_list=[100], sel_list=[0.001])
+run('25', RD_list=[100], sel_list=[0.001])
+run('26', RD_list=[100], sel_list=[0.001])
+run('27', RD_list=[100], sel_list=[0.001])
