b92c65c167f83c14d9a42abcb3b95645c4c55ec8
YCHuang2112 <96855801+YCHuang2112@users.noreply.github.com>
2025-04-30 06:44:28 +0800

    Update sys_rdfilter.h

--- development/rocksdb/include/rocksdb/sys_rdfilter.h
+++ development/rocksdb/include/rocksdb/sys_rdfilter.h
@@ -1059,10 +1059,14 @@ class PLRDF_t{
       if constexpr (std::is_same<KeyType, std::string>::value) {
           // std::cout << "KeyType is std::string\n";
           for(auto it = rd_filter.begin(); it != rd_filter.end(); it++){
-            num += (it->first).size() + (it->second).size(); // size of a range: sizeof(Pair)
+            for(auto it2: *it){
+              num += (it2->first).size() + (it2->second).size(); // size of a range: sizeof(Pair)
+            }
           }
           for(auto it = split_keys.begin(); it != split_keys.end(); it++){
-            num += it->size(); // size of a key: sizeof(KeyType)
+            for(auto it2: *it){
+              num += it2->size(); // size of a key: sizeof(KeyType)
+            }
           }
       } else {
           // std::cout << "KeyType is not std::string\n";
