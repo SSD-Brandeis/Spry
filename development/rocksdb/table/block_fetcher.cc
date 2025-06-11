cbad386963a65153404a65354a818ec245386ded
YCHuang2112sub <ychuang2@bu.edu>
2025-06-11 08:26:32 +0800

    Update block_fetcher.cc

--- development/rocksdb/table/block_fetcher.cc
+++ development/rocksdb/table/block_fetcher.cc
@@ -295,7 +295,7 @@ IOStatus BlockFetcher::ReadBlockContents() {
             opts, handle_.offset(), block_size_with_trailer_, &slice_, nullptr,
             &direct_io_buf_, read_options_.rate_limiter_priority);
         
-        std::cout << "BlockFetcher::ReadBlockContents: " << " file_->use_direct_io() " << " block_type_ = " << block_type_ << " " << " block_size_with_trailer_ = " << block_size_with_trailer_ << " " << " handle_.offset() = " << handle_.offset() << " " << __FILE__ << ":" << __LINE__ << " " << __FUNCTION__ << std::endl;
+        std::cout << "BlockFetcher::ReadBlockContents: " << " file_->use_direct_io() " << " block_type_ = " << int(block_type_) << " " << " block_size_with_trailer_ = " << block_size_with_trailer_ << " " << " handle_.offset() = " << handle_.offset() << " " << __FILE__ << ":" << __LINE__ << " " << __FUNCTION__ << std::endl;
         // //Self Added Start 
         // checking::SystemVerifier::getSystemVerifier()->stop_retrieve_block();
         // //Self Added End        
